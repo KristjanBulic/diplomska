2c1cb6b9f701a035340c794b01590ffe7f0dbce2
bulick <bulic.kristjan@gmail.com>
2023-12-30 23:07:20 +0100

    add logging

--- addition/src/main/java/xyz/bulick/addition/service/AdditionService.java
+++ addition/src/main/java/xyz/bulick/addition/service/AdditionService.java
@@ -1,7 +1,5 @@
 package xyz.bulick.addition.service;
 
-import io.micrometer.tracing.annotation.NewSpan;
-import io.opentelemetry.api.trace.Span;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -13,16 +11,12 @@ public class AdditionService {
 
     private static final Logger log = LoggerFactory.getLogger("AdditionService");
 
-    @NewSpan
     public ResponseDTO add(AdditionDTO dto) {
-        final var span = Span.current();
-        span.setAttribute("args", dto.toString());
 
         log.info("Received: {}", dto);
         final var result = dto.firstNumber() + dto.secondNumber();
         final var response = new ResponseDTO(dto.firstNumber(), dto.secondNumber(), result);
         log.info("Returning: {}", response);
-        span.setAttribute("response", response.toString());
         return response;
     }
 }
